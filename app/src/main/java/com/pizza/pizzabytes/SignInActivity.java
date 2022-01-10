a960c4e074d418cd7dd1348dc8820157e6f1ee90
SYED SAJJAD HAIDER <syedsajjadhaider4@gmail.com>
2022-01-10 19:50:20 +0500

    Pizza Bytes

--- app/src/main/java/com/pizza/pizzabytes/SignInActivity.java
+++ app/src/main/java/com/pizza/pizzabytes/SignInActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +21,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity
 {
-    ImageView facebook, google, twitter, pizza;
+    ImageView facebook, google, pizza;
     TextView pizzatittle,tagline,forget,signuptext;
     EditText user,pass;
     Button login;
-    ConstraintLayout constraintLayout;
+    LinearLayout constraintLayout;
     FirebaseAuth auth;
 
     @Override
@@ -46,22 +47,22 @@ public class SignInActivity extends AppCompatActivity
 
         facebook = (ImageView) findViewById(R.id.fab_fb);
         google = (ImageView) findViewById(R.id.fab_google);
-        twitter = (ImageView) findViewById(R.id.fab_twiter);
+//        twitter = (ImageView) findViewById(R.id.fab_twiter);
         pizza = (ImageView) findViewById(R.id.loginpizza);
-        constraintLayout = (ConstraintLayout) findViewById(R.id.conlayout1);
+        constraintLayout = (LinearLayout) findViewById(R.id.conlayout1);
 
         pizzatittle.setTranslationX(-1000);
         tagline.setTranslationX(-1000);
 
         facebook.setTranslationY(300);
         google.setTranslationY(300);
-        twitter.setTranslationY(300);
+//        twitter.setTranslationY(300);
         pizza.setTranslationY(-600);
         constraintLayout.setTranslationX(1000);
 
         facebook.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
         google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
-        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+//        twitter.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
         pizza.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
         constraintLayout.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(100).start();
 
@@ -145,17 +146,17 @@ public class SignInActivity extends AppCompatActivity
             }
         });
 
-        twitter.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-//                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
-//                startActivity(i);
-                Toast.makeText(getApplicationContext(), "Sign In Against Twitter",Toast.LENGTH_SHORT).show();
-
-            }
-        });
+//        twitter.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+////                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
+////                startActivity(i);
+//                Toast.makeText(getApplicationContext(), "Sign In Against Twitter",Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
 
     }
 
@@ -168,8 +169,7 @@ public class SignInActivity extends AppCompatActivity
                 if (task.isSuccessful())
                 {
                     Toast.makeText(getApplicationContext(), "Welcome to Dashboard " + username, Toast.LENGTH_SHORT).show();
-//                        Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
-//                        startActivity(i);
+//
                 }
                 else
                 {
